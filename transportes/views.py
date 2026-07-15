a5f4822472ae8f6667e9934d66ce1cb857e4cc7e
Dalessandro Santos <dalessandrosantos83@gmail.com>
2026-07-15 14:23:18 -0300

    feat: adiciona horários de domingos e feriados linha urbana

--- transportes/views.py
+++ transportes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Linha
+from .models import Linha, Horario
 
 def home(request):
     return render(request, "transportes/home.html")
@@ -15,11 +15,18 @@ def urbano(request):
             key=lambda h: (h.hora_saida.hour < 4, h.hora_saida)
         )
 
-        grupos = {}
+        grupos_semana = {}
+        grupos_domingo = {}
         for horario in horarios_ordenados:
-            grupos.setdefault(horario.sentido, []).append(horario)
+            destino = (
+                grupos_domingo
+                if horario.frequencia == Horario.Frequencia.DOM_FERIADOS
+                else grupos_semana
+            )
+            destino.setdefault(horario.sentido, []).append(horario)
 
-        linha.grupos_sentido = grupos
+        linha.grupos_semana = grupos_semana
+        linha.grupos_domingo = grupos_domingo
 
     context = {"linhas": linhas}
     return render(request, "transportes/horarios_urbano.html", context)
